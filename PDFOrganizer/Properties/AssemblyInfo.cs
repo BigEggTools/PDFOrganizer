5ce01106fea49ab24c02791797555383a7af9d97
BigEgg <cd_bigegg@hotmail.com>
2019-12-13 15:16:45 -0800

    Update the assembly information

--- PDFOrganizer/Properties/AssemblyInfo.cs
+++ PDFOrganizer/Properties/AssemblyInfo.cs
@@ -1,16 +1,15 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("PDFOrganizer")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("The console interface for PDFOrganizer")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("PDFOrganizer")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyCompany("BigEgg")]
+[assembly: AssemblyProduct("BigEgg Tools PDFOrganizer")]
+[assembly: AssemblyCopyright("Copyright © BigEgg 2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
