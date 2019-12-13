90ca5f6cbda19728463df2b359964aa88a9ebc7e
BigEgg <cd_bigegg@hotmail.com>
2019-12-13 15:16:36 -0800

    Add the merge and split parameter

---
+++ PDFOrganizer/Parameters/MergeParameter.cs
@@ -0,0 +1,17 @@
+﻿using BigEgg.Tools.ConsoleExtension.Parameters;
+
+namespace PDFOrganizer.Parameters
+{
+    [Command("generate", "Generate the markdown file for one folder")]
+    public class MergeParameter
+    {
+        [StringProperty("source", "s", "The source directory", Required = true)]
+        public string Source { get; set; }
+
+        [StringProperty("target", "t", "The target PDF file name")]
+        public string Target { get; set; }
+
+        [StringProperty("prefix", "p", "The prefix filter in the source directory")]
+        public string Prefix { get; set; }
+    }
+}
---
+++ PDFOrganizer/Parameters/SplitParameter.cs
@@ -0,0 +1,17 @@
+﻿using BigEgg.Tools.ConsoleExtension.Parameters;
+
+namespace PDFOrganizer.Parameters
+{
+    [Command("generate", "Generate the markdown file for one folder")]
+    public class SplitParameter
+    {
+        [StringProperty("source", "s", "The source PDF file", Required = true)]
+        public string Source { get; set; }
+
+        [StringProperty("target", "t", "The target directory, same as source PDF file if not specific")]
+        public string Target { get; set; }
+
+        [StringProperty("prefix", "p", "The prefix of the result files")]
+        public string Prefix { get; set; }
+    }
+}
